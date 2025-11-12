c17393b5f5793e4f558c2e55ed2570c466914da1
oscarVanegas <oscarsenavan@gmail.com>
2025-11-12 17:15:45 -0500

    Agrega validación de precio positivo en modelo Producto

--- inventario/models.py
+++ inventario/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)])
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
